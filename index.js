61692ed5bf9ada04aba6612887e56d6bd4a00c93
Daniel Zameshaev <daneelzam@yandex.ru>
2021-01-27 11:14:05 +0300

    add player sprite and movement animation

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ const config = {
 };
 
 const game = new Phaser.Game(config);
+let platforms;
+let player;
 
 function preload() {
   this.load.image('sky', 'assets/sky.png');
@@ -27,13 +29,41 @@ function preload() {
 }
 
 function create() {
-  this.add.image(400, 300, 'sky');
-  platforms = this.physics.add.staticGroup();
-  platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+  this.add.image(400, 300, 'sky');// 400 and 300 - object center position
+  platforms = this.physics.add.staticGroup();// create Static object
+  platforms.create(400, 568, 'ground').setScale(2).refreshBody();// doubling the scale of the "ground" and refresh
 
   platforms.create(600, 400, 'ground');
   platforms.create(50, 250, 'ground');
   platforms.create(750, 220, 'ground');
+
+  player = this.physics.add.sprite(100, 450, 'dude');// create sprite, by default - dynamic obj
+
+  player.setBounce(0.2); // when hitting the ground, the sprite will cover slightly
+  player.setCollideWorldBounds(true); // now the sprite collides with the borders of the world
+
+  this.anims.create({ // movement to the left - animation
+    key: 'left',
+    // use frames 0 to 3 for animation
+    frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+    fremRate: 10, // 10 frames per second
+    repeat: -1, // animation loop
+  });
+
+  this.anims.create({ // movement to the front - animation
+    key: 'turn',
+    // use 4 frames for animation
+    frames: [{ key: 'dude', frame: 4 }],
+    fremRate: 20, // 20 frames per second
+  });
+
+  this.anims.create({ // movement to the right - animation
+    key: 'right',
+    // use frames 5 to 8 for animation
+    frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+    fremRate: 10, // 10 frames per second
+    repeat: -1, // animation loop
+  });
 }
 
 function update() {
