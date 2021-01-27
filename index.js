3b2bec2d8062338ad43d638f722a259b7ea53527
Daniel Zameshaev <daneelzam@yandex.ru>
2021-01-27 12:55:13 +0300

    add stars and picking up stars

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ const config = {
 const game = new Phaser.Game(config);
 let platforms;
 let player;
+let cursors;
+let stars;
 
 function preload() {
   this.load.image('sky', 'assets/sky.png');
@@ -64,7 +66,41 @@ function create() {
     fremRate: 10, // 10 frames per second
     repeat: -1, // animation loop
   });
+
+  cursors = this.input.keyboard.createCursorKeys();
+
+  stars = this.physics.add.group({
+    key: 'star',
+    repeat: 11,
+    setXY: { x: 12, y: 0, stepX: 70 },
+  });
+
+  stars.children.iterate((child) => {
+    child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+  });
+  this.physics.add.collider(player, platforms);// monitors the collision of the player & the ground
+  this.physics.add.collider(stars, platforms);
+
+  this.physics.add.overlap(player, stars, collectStar, null, this);
 }
 
 function update() {
+  if (cursors.left.isDown) {
+    player.setVelocityX(-160);
+    player.anims.play('left', true);
+  } else if (cursors.right.isDown) {
+    player.setVelocityX(160);
+    player.anims.play('right', true);
+  } else {
+    player.setVelocityX(0);
+    player.anims.play('turn');
+  }
+
+  if (cursors.up.isDown && player.body.touching.down) {
+    player.setVelocityY(-330);
+  }
+}
+
+function collectStar(player, star) {
+  star.disableBody(true, true);
 }
